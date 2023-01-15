0f26ea71394bc950e561519d19dbb57c3ad9226c
Patrick Steil <patricksteil@freenet.de>
2023-01-15 19:14:21 +0100

    Add files via upload

---
+++ buildTransferFile.py
@@ -0,0 +1,40 @@
+from scipy import spatial
+import csv
+import geopy.distance
+
+coords = []
+stop_ids = []
+
+avgMPerS = 1.3
+
+def getWalkingDist(coordsA, coordsB):
+	return geopy.distance.geodesic(coordsA, coordsB).m
+
+
+with open("data/s_re/stops.txt", "r", encoding='utf-8-sig') as csvFile:
+	reader = csv.reader(csvFile, skipinitialspace=True)
+	stopIdIndex = -1
+	latIndex = -1
+	lonIndex = -1
+
+	for line in reader:
+		if (stopIdIndex == -1):
+			stopIdIndex = line.index("stop_id")
+			latIndex = line.index("stop_lat")
+			lonIndex = line.index("stop_lon")
+		else:
+			stop_ids.append(line[stopIdIndex])
+			coords.append([float(line[latIndex]), float(line[lonIndex])])
+
+kd_tree = spatial.KDTree(coords)
+
+transfers = []
+
+with open("data/s_re/transfers.txt", "w", encoding='utf-8-sig') as csvFile:
+	writer = csv.writer(csvFile, skipinitialspace=True)
+	writer.writerow(["from_stop_id", "to_stop_id", "transfer_type", "min_transfer_time"])
+
+	for [a, b] in list(kd_tree.query_pairs(r=0.02)):
+		time = int(getWalkingDist(coords[a], coords[b]) / avgMPerS)
+		writer.writerows([stop_ids[a], stop_ids[b], 2, time])
+		writer.writerows([stop_ids[b], stop_ids[a], 2, time])
